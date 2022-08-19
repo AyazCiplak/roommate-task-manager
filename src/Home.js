ef8d663101e25e7f79a626eb91b4bb268bf72341
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-08-19 16:35:22 -0400

    Added user array list function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+    const [users, setUsers] = useState([
+        { name: "Name 1", id: 1},
+        { name: "Name 2", id: 2},
+        { name: "Name 3", id: 3}
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            <h2>Users</h2>
+            {
+            //Note: Preview just shows user name, clicking on user should show more info when it is added
+            users.map((user) => (
+                <div className="preview" key={user.id}>
+                    <h3>{ user.name }</h3>
+                </div>
+            ))}
         </div>
      );
 }
