5cfb11a4b3e965895b67c64ce62d1b290a1dadae
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-09-03 12:29:21 -0400

    Added error catch and display functions

--- src/Home.js
+++ src/Home.js
@@ -15,25 +15,35 @@ const Home = () => {
     const [tasks, setTasks] = useState(null)
 
     const [tasksPending, setTasksPending] = useState(true);
+    const [error, setError] = useState(null);
 
     //Fetches data from taskdb JSON file (note: must be running on port 8000)
     useEffect(() => {
         fetch('http://localhost:8000/tasks')
         .then(res => {
+            if(!res.ok) {
+                throw Error("Data could not be fetched for that resource.")
+            }
             return res.json()
         })
         .then(data => {
             setTasksPending(false);
             setTasks(data); 
+            setError(null);
         })
+        .catch(err => {
+            setTasksPending(false);
+            setError(err.message);
+    })
     }, []);
 
     //Note: && does not display the second element if the first one is false
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             <UserList users={users}/>
             { tasksPending && <div>Loading tasks...</div> }
-            { tasks && <TaskList tasks={tasks}/> }
+            { tasks && <TaskList tasks={ tasks }/> }
         </div>
      );
 }
