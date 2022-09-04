cac22056ee6daef3e1b2c2a7072fe2c56eaf3b4c
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-09-04 14:27:17 -0400

    Linked new task form submission to JSON database

--- src/CreateTask.js
+++ src/CreateTask.js
@@ -1,19 +1,50 @@
+import {useState} from 'react';
+
 const CreateTask = () => {
+    
+    //Empty states to be filled with info from input form
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [responsible, setResponsible] = useState('None');
+    const [date, setDate] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const task = { title, description, responsible, date };
+
+        fetch('http://localhost:8000/tasks', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(task)
+            
+        }).then (() => {
+            console.log("New task added");
+        })
+    }
+    
     return ( 
         <div className="create-task">
             <h2>Add a new task</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Task title: </label>
                 <input 
                 type="text"
                 required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Description: </label>
                 <textarea
-                  required 
+                  required
+                  value={description} 
+                  onChange={(e) => setDescription(e.target.value)}
                 ></textarea>
                 <label>Responsible: </label>
-                <select>
+                <select
+                 value = {responsible}
+                 onChange = {(e) => setResponsible(e.target.value)}
+                >
                     {// Options below are just samples, will have to connect to users database
                     }
                     <option value="Ayaz">Ayaz</option>
@@ -24,6 +55,8 @@ const CreateTask = () => {
                 <label>Due date: </label>
                 <input
                 type="text"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
                 />
                 <button>Add task</button>
             </form>
