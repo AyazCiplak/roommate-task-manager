0279936fad21cb1f066467a651b101dc3f209cb0
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-09-18 19:26:55 -0400

    Modifications to fetchData generalization

--- src/TaskDetails.js
+++ src/TaskDetails.js
@@ -5,7 +5,7 @@ import useFetch from './useFetch';
 const TaskDetails = () => {
     
     const { id } = useParams();
-    const { data, dataPending , error } = useFetch('http://localhost:8000/tasks/' + id);
+    const { data : task, dataPending , error } = useFetch('http://localhost:8000/tasks/' + id);
     const history = useHistory();
 
     const handleClick = () => {
@@ -21,12 +21,12 @@ const TaskDetails = () => {
             <h2>Task: { id }</h2>
             { dataPending && <div>Loading...</div> }
             { error && <div>{ error }</div> }
-            { data && (
+            { task && (
                 <article>
-                    <h3>Title: { data.title }</h3>
-                    <h4>Description: { data.description }</h4>
-                    <h4>Responsible: { data.responsible }</h4>
-                    <h4>Due date: { data.date }</h4>
+                    <h3>Title: { task.title }</h3>
+                    <h4>Description: { task.description }</h4>
+                    <h4>Responsible: { task.responsible }</h4>
+                    <h4>Due date: { task.date }</h4>
                     <button onClick={handleClick}>Delete Task</button>
                 </article>
             )}
